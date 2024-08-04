fe2d6766308699ec011b31c54cabe7c1d4d40043
tomo1227 <tomo.soccer1227@gmail.com>
2024-08-05 02:58:44 +0900

    Initial Commit

---
+++ Gakakun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gakakun
+//
+//  Created by Tomoki Ota on 2024/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gakakun/GakakunApp.swift
@@ -0,0 +1,17 @@
+//
+//  GakakunApp.swift
+//  Gakakun
+//
+//  Created by Tomoki Ota on 2024/08/05.
+//
+
+import SwiftUI
+
+@main
+struct GakakunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
